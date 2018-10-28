c3b112d1fefabb0116576d33af17ba662157f9ba
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-10-28 23:21:55 +0530

    Removed condition for diamond of even height from main file

--- diamond.js
+++ diamond.js
@@ -5,7 +5,6 @@ const { generateDiamond } = lib;
 const main = function() {
   let type = process.argv[2];
   let height = +process.argv[3];
-  if(height % 2 == 0){ height = height -1 };
   let diamond = generateDiamond(type, height);
   console.log(diamond);
 }
