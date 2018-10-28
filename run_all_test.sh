66a300ee29ea38de9bb7e9b2ffd7311dc35a525c
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-10-28 14:11:44 +0530

    File for running all test simultaneously added

---
+++ run_all_test.sh
@@ -0,0 +1,5 @@
+./run_test.sh ./source/rectangle.js ./test/rectangles_input_file ./test/rectangles_output_file
+
+./run_test.sh ./source/triangle.js ./test/triangles_input_file ./test/triangles_output_file
+
+./run_test.sh ./source/diamond.js ./test/diamonds_input_file ./test/diamonds_output_file
