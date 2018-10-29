f2dd2de84730d6ce38e0791f71692b9c949d4161
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-10-29 13:07:46 +0530

    Introduced structure to repository according to functionality

--- run_all_test.sh
+++
@@ -1,5 +0,0 @@
-./run_test.sh ./rectangle.js ./test/rectangles_input_file ./test/rectangles_output_file
-
-./run_test.sh ./triangle.js ./test/triangles_input_file ./test/triangles_output_file
-
-./run_test.sh ./diamond.js ./test/diamonds_input_file ./test/diamonds_output_file
