bf5b6cd6ec59276b2093f7646789ef8098a4160a
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-10-29 16:27:06 +0530

    Modified run_all_test to run it from root

--- appTest/run_all_test.sh
+++ appTest/run_all_test.sh
@@ -1,5 +1,5 @@
-./run_test.sh ../rectangle.js ../appTestData/rectangles_input_file ../appTestData/rectangles_output_file
+appTest/run_test.sh ./rectangle.js ./appTestData/rectangles_input_file ./appTestData/rectangles_output_file
 
-./run_test.sh ../triangle.js ../appTestData/triangles_input_file ../appTestData/triangles_output_file
+appTest/run_test.sh ./triangle.js ./appTestData/triangles_input_file ./appTestData/triangles_output_file
 
-./run_test.sh ../diamond.js ../appTestData/diamonds_input_file ../appTestData/diamonds_output_file
+appTest/run_test.sh ./diamond.js ./appTestData/diamonds_input_file ./appTestData/diamonds_output_file
