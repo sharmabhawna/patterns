df86440a9e23c970c1b41779ec5ace8a09f7a5e4
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-10-31 16:19:36 +0530

    Initial commit

--- source/patternsUtility.js
+++ source/patternsUtility.js
@@ -10,70 +10,85 @@ const getInput  = function() {
   return { type : type, width : width, height : height };
 }
 
-  const repeatCharacter = function(character, times) {
-    let line = "";
-    for(let count = 0; count < times; count++){
-      line = line + character;
-    }
-    return line;
+const repeatCharacter = function(character, times) {
+  let result = "";
+  for(let count = 0; count < times; count++){
+    result = result + character;
   }
+  return result;
+}
 
-  const generateLine = function(leftEndSymbol, middleSymbol, rightEndSymbol, length) {
+const generateRectangleParameters = function(width, height) {
+  return new Array(height).fill(width);
+}
+
+const generateLine = function(leftEndSymbol, middleSymbol, rightEndSymbol) {
+  return function(length){
+    let line = "";
     let leftBorderWidth = 1 % (length+1);
     let rightBorderwidth = 1 % length;
-    let line = repeatCharacter(leftEndSymbol, leftBorderWidth);
+    line = repeatCharacter(leftEndSymbol, leftBorderWidth);
     line = line + repeatCharacter(middleSymbol, length-2);
     line = line + repeatCharacter(rightEndSymbol, rightBorderwidth);
-    return line;
+  return line;
   }
+}
 
-  const upperHalf = function(diamondType, height) {
-    let pattern = "";
-    let delimiter = "";
-    let line = "";
-    top = repeatCharacter(" ", (height-1)/2);
-    top = top + repeatCharacter("*", 1) ;
-    for(let row = 3; row <= height-2; row += 2){
-      line = repeatCharacter(" ", (height-row)/2);
-      if(diamondType == "filled"){
-        line = line + generateLine("*", "*", "*", row); 
-      } 
-      if(diamondType == "hollow"){
-        line = line + generateLine("*", " ", "*", row);
-      }
-      if(diamondType == "angled"){
-        line = line + generateLine("/", " ", "\\", row);
-      }
-      pattern = pattern + delimiter + line;
-      delimiter = "\n";
+let generateStarLine = generateLine("*","*","*");
+let generateDashLine = generateLine("-","-","-");
+let generateHollowLine = generateLine("*"," ","*");
+
+const upperHalf = function(diamondType, height) {
+  let pattern = "";
+  let delimiter = "";
+  let line = "";
+  top = repeatCharacter(" ", (height-1)/2);
+  top = top + repeatCharacter("*", 1) ;
+  for(let row = 3; row <= height-2; row += 2){
+    line = repeatCharacter(" ", (height-row)/2);
+    if(diamondType == "filled"){
+      line = line + generateLine("*", "*", "*", row); 
+    } 
+    if(diamondType == "hollow"){
+      line = line + generateLine("*", " ", "*", row);
     }
-    return top + delimiter + pattern;
-  }
-
-  const bottomHalf = function(diamondType, height) {
-    let pattern = "";
-    let delimiter = "";
-    for(let row = height-2; row >= 3; row -= 2){
-      let line = repeatCharacter(" ", (height-row)/2);
-      if(diamondType == "filled"){
-        line = line + generateLine("*", "*", "*", row); 
-      } 
-      if(diamondType == "hollow"){
-        line = line + generateLine("*", " ", "*", row);
-      }
-      if(diamondType == "angled"){
-        line = line + generateLine("\\", " ", "/", row);
-      }
-      pattern = pattern + delimiter + line;
-      delimiter = "\n";
+    if(diamondType == "angled"){
+      line = line + generateLine("/", " ", "\\", row);
     }
-    let bottom = repeatCharacter(" ", (height-1)/2);
-    bottom = bottom + repeatCharacter("*", 1);
-    return pattern + delimiter + bottom;
+    pattern = pattern + delimiter + line;
+    delimiter = "\n";
   }
+  return top + delimiter + pattern;
+}
 
-  exports.getInput = getInput;
-  exports.repeatCharacter = repeatCharacter;
-  exports.generateLine = generateLine;
-  exports.upperHalf = upperHalf;
-  exports.bottomHalf = bottomHalf;
+const bottomHalf = function(diamondType, height) {
+  let pattern = "";
+  let delimiter = "";
+  for(let row = height-2; row >= 3; row -= 2){
+    let line = repeatCharacter(" ", (height-row)/2);
+    if(diamondType == "filled"){
+      line = line + generateLine("*", "*", "*", row); 
+    } 
+    if(diamondType == "hollow"){
+      line = line + generateLine("*", " ", "*", row);
+    }
+    if(diamondType == "angled"){
+      line = line + generateLine("\\", " ", "/", row);
+    }
+    pattern = pattern + delimiter + line;
+    delimiter = "\n";
+  }
+  let bottom = repeatCharacter(" ", (height-1)/2);
+  bottom = bottom + repeatCharacter("*", 1);
+  return pattern + delimiter + bottom;
+}
+
+exports.getInput = getInput;
+exports.generateRectangleParameters = generateRectangleParameters;
+exports.repeatCharacter = repeatCharacter;
+exports.generateLine = generateLine;
+exports.generateStarLine = generateStarLine;
+exports.generateDashLine = generateDashLine;
+exports.generateHollowLine = generateHollowLine;
+exports.upperHalf = upperHalf;
+exports.bottomHalf = bottomHalf;
--- unitTest/patternsUtilTest.js
+++ unitTest/patternsUtilTest.js
@@ -2,7 +2,11 @@ const assert = require('assert');
 const utilLib = require('../source/patternsUtility.js');
 
 const { repeatCharacter,
+  generateRectangleParameters,
   generateLine,
+  generateStarLine,
+  generateDashLine,
+  generateHollowLine,
   upperHalf,
   bottomHalf } = utilLib;
 
@@ -14,17 +18,47 @@ assert.equal(repeatCharacter("-", 1), "-");
 assert.equal(repeatCharacter("*", 5), "*****");
 assert.equal(repeatCharacter("-", 5), "-----");
 
+//Tests for generateRectangleParameters :
+assert.deepEqual(generateRectangleParameters(1,1), [1]);
+assert.deepEqual(generateRectangleParameters(1,2), [1,1]);
+assert.deepEqual(generateRectangleParameters(2,1), [2]);
+assert.deepEqual(generateRectangleParameters(2,2), [2,2]);
+
 //Tests for generateLine :
-assert.equal(generateLine("*","*","*", 1), "*");
-assert.equal(generateLine("*","*","*", 5), "*****");
-assert.equal(generateLine("-","-","-", 1), "-");
-assert.equal(generateLine("-","-","-", 5), "-----");
-assert.equal(generateLine("*"," ","*", 1), "*");
-assert.equal(generateLine("*"," ","*", 5), "*   *");
-assert.equal(generateLine("/"," ","\\", 1), "/");
-assert.equal(generateLine("/"," ","\\", 5), "/   \\");
-assert.equal(generateLine("\\"," ","/", 1), "\\");
-assert.equal(generateLine("\\"," ","/", 5), "\\   /");
+let starLine = generateLine("*","*","*");
+assert.equal(starLine(1), "*");
+assert.equal(starLine(5), "*****");
+
+let dashLine = generateLine("-","-","-");
+assert.equal(dashLine(1), "-");
+assert.equal(dashLine(5), "-----");
+
+let hollowLine = generateLine("*"," ","*");
+assert.equal(hollowLine(1), "*");
+assert.equal(hollowLine(5), "*   *");
+
+assert.equal(generateLine("/"," ","\\")(1), "/");
+assert.equal(generateLine("/"," ","\\")(5), "/   \\");
+assert.equal(generateLine("\\"," ","/")(1), "\\");
+assert.equal(generateLine("\\"," ","/")(5), "\\   /");
+
+//Tests for generateStarLine :
+assert.deepEqual(generateStarLine(0), "");
+assert.deepEqual(generateStarLine(1), "*");
+assert.deepEqual(generateStarLine(2), "**");
+assert.deepEqual(generateStarLine(3), "***");
+
+//Tests for generateDashLine :
+assert.deepEqual(generateDashLine(0), "");
+assert.deepEqual(generateDashLine(1), "-");
+assert.deepEqual(generateDashLine(2), "--");
+assert.deepEqual(generateDashLine(3), "---");
+
+//Tests for generateHollowLine :
+assert.deepEqual(generateHollowLine(0), "");
+assert.deepEqual(generateHollowLine(1), "*");
+assert.deepEqual(generateHollowLine(2), "**");
+assert.deepEqual(generateHollowLine(3), "* *");
 
 //Tests for upperHalf :
 assert.equal(upperHalf("filled", 1), "*");
